0814f38e55b7ece6c94947753751a39a61efd5bc
Frank Errickson <frankerrickson@berkeley.edu>
2017-01-24 12:57:10 -0800

    Change parameters to match python version of FAIR.

--- src/fair.jl
+++ src/fair.jl
@@ -35,20 +35,20 @@ function constructfair(;nsteps=250, scenario="rcp8.5", start_year = 1900)
     # ---------------------------------------------
     # Set component parameters
     # ---------------------------------------------
-    setparameter(m, :carboncycle, :C0, 278.05158)
+    setparameter(m, :carboncycle, :C0, 278.0)
     setparameter(m, :carboncycle, :r0, 35.0)
     setparameter(m, :carboncycle, :rC, 0.02)
-    setparameter(m, :carboncycle, :rT, 8.5)
+    setparameter(m, :carboncycle, :rT, 4.5)
     setparameter(m, :carboncycle, :a, [0.2173, 0.2240, 0.2824, 0.2763])
-    setparameter(m, :carboncycle, :τ, [10.0^8, 394.4, 36.54, 4.304])
+    setparameter(m, :carboncycle, :τ, [10.0^6, 394.4, 36.54, 4.304])
     setparameter(m, :carboncycle, :E, E)
 
     setparameter(m, :radiativeforcing, :C0, 278.05158)
     setparameter(m, :radiativeforcing, :F2x, 3.74/log(2))
     setparameter(m, :radiativeforcing, :Fext, Fext)
 
-    setparameter(m, :temperature, :q, [0.46, 0.27])
-    setparameter(m, :temperature, :d, [8.4, 409.5])
+    setparameter(m, :temperature, :q, [0.33, 0.41])
+    setparameter(m, :temperature, :d, [239.0, 4.1])
 
     # -----------------------------------------------
     # Create necessary connections between components
